f98b3d9498f7522921dd1818dbec86e956c216c5
Rafael Ferreira <rafaelferreoli@gmail.com>
2023-12-20 21:25:06 -0300

    algoritmo da mochila

---
+++ src/main/java/com/mycompany/problemamochila/ProblemaMochila.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.problemamochila;
+
+/**
+ *
+ * @author rafae
+ */
+import view.FrHome;
+
+ 
+public class ProblemaMochila {
+
+    public static void main(String[] args) {
+        FrHome fr = new FrHome();
+        fr.setVisible(true);
+
+    }
+}
